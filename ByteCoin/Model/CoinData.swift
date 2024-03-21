e316f65b0ce66e7e47feef9045cb1d2548a90813
Ana Ptskialadze <anukiptskialadze@gmai.com>
2024-03-21 21:27:29 +0100

    ByteCoin app 1st version.

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Ana Ptskialadze on 09.12.23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
